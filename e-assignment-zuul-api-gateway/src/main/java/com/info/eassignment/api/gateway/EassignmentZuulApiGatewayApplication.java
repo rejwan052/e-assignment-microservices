4c374ba75de966dba65edb94cbda26792ccf7e17
Rejwan Ahmed <rejwan052@gmail.com>
2019-10-20 00:36:20 +0600

    Spring security at zuul api gateway

--- e-assignment-zuul-api-gateway/src/main/java/com/info/eassignment/api/gateway/EassignmentZuulApiGatewayApplication.java
+++ e-assignment-zuul-api-gateway/src/main/java/com/info/eassignment/api/gateway/EassignmentZuulApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.info.eassignment.eassignmentzuulapigateway;
+package com.info.eassignment.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- e-assignment-zuul-api-gateway/src/test/java/com/info/eassignment/api/gateway/DemoApplicationTests.java
+++ e-assignment-zuul-api-gateway/src/test/java/com/info/eassignment/api/gateway/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.info.eassignment.eassignmentzuulapigateway;
+package com.info.eassignment.api.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
