ecbc6268d652c52865197f7d80dc0d4d47d3f31e
Rejwan Ahmed <rejwan.052@gmail.com>
2019-10-23 23:28:58 +0600

    ## spring boot actuator & mysql database integration

--- e-assignment-user-service/src/main/java/com/info/eassignment/users/controller/UsersController.java
+++ e-assignment-user-service/src/main/java/com/info/eassignment/users/controller/UsersController.java
@@ -32,7 +32,7 @@ public class UsersController {
 
     @GetMapping("/users")
     public String status() {
-        return "Working... on port " + env.getProperty("local.server.port");
+        return "Working... on port " + env.getProperty("local.server.port")+" ,with token "+ env.getProperty("token.secret");
     }
 
     @PostMapping(value = "/users")
--- e-assignment-zuul-api-gateway/src/main/java/com/info/eassignment/api/gateway/security/WebSecurity.java
+++ e-assignment-zuul-api-gateway/src/main/java/com/info/eassignment/api/gateway/security/WebSecurity.java
@@ -27,6 +27,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers(environment.getProperty("api.h2-console.url.path")).permitAll()
+                .antMatchers(environment.getProperty("api.zuul.actuator.url.path")).permitAll()
+                .antMatchers(environment.getProperty("api.users.actuator.url.path")).permitAll()
                 .antMatchers(HttpMethod.POST,environment.getProperty("api.registration.url.path")).permitAll()
                 .antMatchers(HttpMethod.POST,environment.getProperty("api.login.url.path")).permitAll()
                 .anyRequest().authenticated()
