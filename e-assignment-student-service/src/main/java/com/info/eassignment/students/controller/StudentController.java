34c467df29c8fd0cb901c103ff5fa9fd1b70e9d4
Rajwan AHmed <rejwan052@gmail.com>
2020-04-18 00:36:08 +0600

    ## implement zipkin at students-service

--- e-assignment-student-service/src/main/java/com/info/eassignment/students/controller/StudentController.java
+++ e-assignment-student-service/src/main/java/com/info/eassignment/students/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.info.eassignment.students.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class StudentController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/students")
     public String status() {
-        return "Student service working...";
+        return "Student service working..."+env.getProperty("local.server.port");
     }
 
 }
